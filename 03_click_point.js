21cf8e7636b34e617d60e572fff5c82a271f5303
pezy <urbancpz@gmail.com>
2016-04-22 09:15:22 +0800

    refactoring that abstract init function to glut.js.

--- 03_click_point.js
+++ 03_click_point.js
@@ -1,4 +1,3 @@
-var gl; // A global variable for the WebGL context.
 var g_points = []; // The array for a mouse press.
 var g_colors = []; // The array to store the color of a point.
 
@@ -17,62 +16,20 @@ var FSHADER_SOURCE = // Fragment shader program
 "   gl_FragColor = u_FragColor;\n" +
 "}\n";
 
-function initWebGL(canvas) {
-    gl = null;
-    
-    try {
-        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    } catch (e) {
-        
-    }
-    
-    if (!gl)
-    {
-        alert("Unable to initialize WebGL. Your browser may not support it.");
-        gl = null;
-    }
-}
-
-function initShaders()
-{
-    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, VSHADER_SOURCE);
-    gl.compileShader(vertexShader);
-    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(vertexShader));
-    }
-    
-    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, FSHADER_SOURCE);
-    gl.compileShader(fragmentShader);
-    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(fragmentShader));
-    }
-    
-    var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-    
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Unable to initialize the shader program.");
-    }
-    
-    gl.useProgram(shaderProgram);
-    gl.program = shaderProgram;
-}
-
 function init() {
     var canvas = document.getElementById("webgl");
     
-    initWebGL(canvas);
+    var gl = initWebGL(canvas);
     
     if (!gl) {
         console.log('Failed to get the rendering context for WebGL.');
         return;
     }
     
-    initShaders();
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to create shader program.');
+        return;
+    }
     
     var a_Position = gl.getAttribLocation(gl.program, "a_Position");
     if (a_Position < 0) {
